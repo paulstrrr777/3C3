282b22c9854ef5794c03edb66fa8b7afc0621687
Pavel Strelnikov <strelnikov_p@list.ru>
2022-04-18 02:44:18 +0500

    1 commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+namespace C3
+{
+    class Programm
+    {   
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+
+    }
+    
+}
